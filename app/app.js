92116aabfce29011ea4a5c57a181f98826819e82
김수봉 <kimsubong@gimsubong-ui-MacBookAir.local>
2022-09-26 22:29:40 +0900

    src 폴더로 분류

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ const home = require("./routes/home");
 // 앱 세팅
 app.set("views", "./views");
 app.set("view engine", "ejs");
-
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
 
