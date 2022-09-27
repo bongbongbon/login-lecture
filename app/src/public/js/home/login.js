b5201dbaf6d64e486b11a391cc8609e4b439a7e1
김수봉 <kimsubong@gimsubong-ui-MacBookAir.local>
2022-09-27 19:54:13 +0900

    돔을 이용하여 HTML객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id  = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    logingBtn = document.querySelector("button");
+
+logingBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req);
+}
