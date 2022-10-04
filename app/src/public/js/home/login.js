d916d7bda15d3039f936802335b221f66a147132
김수봉 <kimsubong@gimsubong-ui-MacBookAir.local>
2022-10-04 11:25:43 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const { json } = require("body-parser");
+const { application } = require("express");
+
 const id  = document.querySelector("#id"),
     psword = document.querySelector("#psword"),
     logingBtn = document.querySelector("button");
@@ -8,8 +11,15 @@ logingBtn.addEventListener("click", login);
 
 function login(){
     const req = {
-        id: id.value,
+        id: id.value, 
         psword: psword.value,
     };
-    console.log(req);
 }
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req),
+    });
